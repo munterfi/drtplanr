fa8b37954cfa02cfcd2076557322e012dbed17da
munterfinger <lyn@hispeed.ch>
2020-03-12 20:49:48 +0100

    Change routing from HERE to R internal dodgr routing based on OSM

--- R/drtm.R
+++ R/drtm.R
@@ -1,15 +1,70 @@
 ### Model class definition
-drt_model <- function(model_name, n_sta, sta, seg, pop, calc_energy = e_walkDrive_pop, router = NULL) {
-  tmessage("Initialize model '%s'" %>% sprintf(model_name))
-  if (is.null(router)) {
-    router <- "NA"
-    message("Pseudo-routing with sf::st_distance() --> For valid results, set up here or OSRM!")
-  }
+drt_drtm <- function(model_name, aoi, pop, n_vir, m_seg = 100,
+                     calc_energy = e_walkDrive_pop) {
+  aoi <- aoi %>% sf::st_transform(4326)
+  pop <- pop %>% sf::st_transform(4326)
+  
+  # Get OSM data
+  tmessage("Get data from OSM: Streets 'roa', bus stops and rail stations 'sta'")
+  bb <- aoi %>% drt_osm_bb()
+  roa <- dodgr::dodgr_streetnet(bbox = bb)
+  
+  # Stations
+  sta_rail <-
+    osmdata::opq(bbox = bb) %>%
+    osmdata::add_osm_feature(key = "railway", value = "station") %>%
+    osmdata::osmdata_sf() %>% 
+    .$osm_points
+  sta_rail$type = "rail"
+  
+  sta_bus <-
+    osmdata::opq(bbox = bb) %>%
+    osmdata::add_osm_feature(key = "highway", value = "bus_stop") %>%
+    osmdata::osmdata_sf() %>% 
+    .$osm_points
+  sta_bus$type = "bus"
+  
+  sta <- rbind(
+    sta_rail[, c("type", "geometry")],
+    sta_bus[, c("type", "geometry")]
+  )
+
+  # Create routing graphs
+  tmessage("Create routing graphs for 'foot' and 'mcar'")
+  foot <- dodgr::weight_streetnet(roa, wt_profile = "foot")
+  mcar <- dodgr::weight_streetnet(roa, wt_profile = "motorcar")
+  
+  # Mask layers to AOI
+  tmessage("Mask layers 'roa', 'pop' and 'sta' by 'aoi'")
+  roa <- roa[!roa$highway %in% c("footway", "path", "cycleway", "steps", "track", "service"), ]
+  roa <- drt_mask(roa, aoi)
+  pop <- drt_mask(pop, aoi)
+  sta <- drt_mask(sta, aoi)
+  
+  # Split roads in segments
+  tmessage("Extract possible station locations 'seg' from street segments")
+  seg <- drt_roa_seg(roa, m_seg = 100)
+  
+  # Route driving times from all segments to the rail stations
+  tmessage("Route driving times of 'seg' to the rail stations")
+  car_time <- drt_route_matrix(
+    orig = sta[sta$type == "rail", ],
+    dest = seg,
+    graph = mcar
+  ) %>% 
+    .[, .(travelTime = min(travelTime)), by = destIndex]
+  seg$carTime <- car_time$travelTime
+  seg <- seg[!is.na(seg$carTime), ]
+  
   # Existing stations
-  idx_const <- sf::st_nearest_feature(sta, seg)
+  tmessage("Map stations 'sta' to segments 'seg' and set as constant")
+  idx_const <- suppressMessages(
+    sf::st_nearest_feature(sta, seg)
+  )
   
   # Random sample
   n_seg <- nrow(seg)
+  n_sta <- n_vir + length(idx_const)
   idx <- c(.sample_exclude(1:n_seg, n_sta, idx_const), idx_const)
 
   ## Create model obj
@@ -21,15 +76,22 @@ drt_model <- function(model_name, n_sta, sta, seg, pop, calc_energy = e_walkDriv
     idx = idx,
     e = data.table::data.table(
       iteration = 0,
-      value = sum(calc_energy(idx, seg, pop, router))
+      value = sum(calc_energy(idx, seg, pop, foot))
     ),
     params = list(
       n_sta = n_sta,
+      n_vir = n_vir,
       n_seg = nrow(seg),
-      router = router, 
+      m_seg = m_seg,
       calc_energy = calc_energy
     ),
-    data = list(
+    route = list(
+      foot = foot,
+      mcar = mcar
+    ),
+    layer = list(
+      aoi = aoi,
+      roa = roa,
       seg = seg,
       pop = pop
     )
@@ -51,33 +113,72 @@ print.drtm <- function(obj) {
 }
 
 
+## OSM Layers
+drt_osm_bb = function(x, ...) UseMethod("drt_osm_bb")
+
+drt_osm_bb.sf <- function(aoi) {
+  sf_bb <- aoi %>% sf::st_transform(4326) %>% sf::st_bbox()
+  osm_bb <- matrix(sf_bb, 2)
+  colnames(osm_bb) <- c("min", "max")
+  rownames(osm_bb) <- c("x", "y")
+  osm_bb
+}
+
+
+## Spatial
+drt_mask = function(x, ...) UseMethod("drt_mask")
+
+drt_mask.sf <- function(layer, aoi) {
+  suppressMessages(
+    suppressWarnings(
+      sf::st_intersection(layer, sf::st_geometry(aoi))
+    )
+  )
+}
+
+drt_roa_seg = function(x, ...) UseMethod("drt_roa_seg")
+
+drt_roa_seg.sf <- function(roa, m_seg) {
+  seg <- 
+    roa %>% 
+    sf::st_union() %>% 
+    sf::st_segmentize(units::set_units(m_seg, m)) %>%
+    sf::st_cast("POINT") %>% 
+    sf::st_as_sf()
+  colnames(seg) <- c("geometry")
+  sf::st_geometry(seg) <- "geometry"
+  seg$id <- seq(1, nrow(seg))
+  seg[, c("id", "geometry")]
+}
+
+
+### Routing
+drt_route_matrix <- function(orig, dest, graph) {
+  m <- dodgr::dodgr_times(
+    graph = graph,
+    from = sf::st_coordinates(orig),
+    to = sf::st_coordinates(dest)
+  )
+  nn <- expand.grid(1:nrow(m), 1:ncol(m)) %>% data.table::data.table()
+  colnames(nn) <- c("origIndex", "destIndex")
+  nn$travelTime <- m[cbind(nn$origIndex, nn$destIndex)] %>% as.numeric()
+  nn[order(nn$origIndex, nn$destIndex), ]
+}
+
+
 ### Sampling
 .sample_exclude <- function(x, size, const) {
   sampling <- TRUE
   while (sampling) {
     idx <- sample(x, size, replace = FALSE)
     if (any(idx %in% const)) {
-      message("\r  Duplicate stations: Resampling...                          ")
+      sampling <- TRUE
+      #message("\r  Duplicate stations: Resampling...                          ")
     } else {
       sampling <- FALSE
     }
   }
   return(idx)
-}
-
-
-### Routing
-drt_route_matrix <- function(orig, dest, mode, router) {
-  if (router == "here") {
-    nn <- hereR::route_matrix(orig, dest, mode = mode) %>%
-      as.data.table()
-  } else {
-    m <- sf::st_distance(orig, dest, by_element = FALSE)
-    nn <- expand.grid(1:nrow(m), 1:ncol(m)) %>% data.table()
-    colnames(nn) <- c("origIndex", "destIndex")
-    nn$travelTime <- m[cbind(nn$origIndex, nn$destIndex)] %>% as.numeric()
-    nn[order(nn$origIndex, nn$destIndex), ]
-  }
 }
 
 
@@ -93,14 +194,14 @@ e_walk_pop <- function(idx, seg, pop, router) {
 }
 
 # Global energy: sum((walk+5*drive)/pop)
-e_walkDrive_pop <- function(idx, seg, pop, router) {
+e_walkDrive_pop <- function(idx, seg, pop, graph) {
   # Route
-  rts <- drt_route_matrix(seg[idx, ], pop, mode = "pedestrian", router = router)
+  rts <- drt_route_matrix(seg[idx, ], pop, graph = graph)
   # Get nearest station
   nn <- rts[, .SD[which.min(travelTime)], by = list(destIndex)]
   nn$pop <- pop[nn$destIndex, ]$n
   nn$carTime <- seg[idx[nn$origIndex], ]$carTime
-  return(nn[, sum((travelTime+5*carTime)/(pop)), by = list(origIndex)])
+  return(nn[, sum((travelTime*carTime)/(pop)), by = list(origIndex)])
 }
 
 
@@ -123,7 +224,7 @@ drt_iterate.drtm <- function(obj, n_iter) {
     idx_old <- obj$idx[idx_new_pos]
     idx_new <- .sample_exclude(1:obj$params$n_seg, 1, obj$idx)
     obj$idx[idx_new_pos] <- idx_new
-    e_new <- sum(obj$params$calc_energy(obj$idx, obj$data$seg, obj$data$pop, obj$params$router))
+    e_new <- sum(obj$params$calc_energy(obj$idx, obj$layer$seg, obj$layer$pop, obj$route$foot))
     cat(sprintf("\r  Iteration: %s, e0: %s, e1: %s \r",
                 i - 1, round(e_old, 1), round(e_new, 1)))
     if (e_old > e_new) {
@@ -167,9 +268,9 @@ drt_import.character <- function(file_name) {
 
 
 ### Graphics
-drt_energy_plot = function(x, ...) UseMethod("drt_energy_plot")
+drt_plot = function(x, ...) UseMethod("drt_plot")
 
-drt_energy_plot.drtm <- function(obj) {
+drt_plot <- function(obj) {
   tmessage("Print energy plot")
   p <-
     ggplot2::ggplot(obj$e, ggplot2::aes(x = iteration, y = value)) +
@@ -181,22 +282,35 @@ drt_energy_plot.drtm <- function(obj) {
   p
 }
 
-drt_station_map = function(x, ...) UseMethod("drt_station_map")
+drt_map = function(x, ...) UseMethod("drt_map")
 
-drt_station_map.drtm <- function(obj) {
+drt_map.drtm <- function(obj) {
   tmessage("Print station map")
-  const <- obj$data$seg[obj$idx_const, ]
+  const <- obj$layer$seg[obj$idx_const, ]
   const$station <- "Existing"; const$size <- 6
-  start <- obj$data$seg[obj$idx_start[!obj$idx_start %in% obj$idx_const], ]
+  start <- obj$layer$seg[obj$idx_start[!obj$idx_start %in% obj$idx_const], ]
   start$station <- "Initial"; start$size <- 3
-  optim <- obj$data$seg[obj$idx[!obj$idx %in% obj$idx_const], ]
+  optim <- obj$layer$seg[obj$idx[!obj$idx %in% obj$idx_const], ]
   optim$station <- "Optimized"; optim$size <- 6
   stations <- rbind(const, start, optim)
   m <-
     mapview::mapview(
+      obj$layer$aoi, alpha = 0.25, alpha.region = 0, color = "black", lwd = 2,
+      legend = FALSE, layer.name = "AOI", label = "AOI", homebutton = TRUE
+    ) +
+    mapview::mapview(
+      obj$layer$roa, alpha = 0.25, color = "black",
+      legend = FALSE, layer.name = "Street network", homebutton = FALSE
+    ) +
+    mapview::mapview(
+      obj$layer$seg, alpha = 0, alpha.region = 0.25,
+      color = "black", col.region = "black", cex = 1,
+      legend = FALSE, layer.name = "Segments", homebutton = FALSE
+    ) +
+    mapview::mapview(
       stations, alpha = 0, zcol = "station",
       cex = stations$size, col.regions = c("black", "blue", "red"),
-      layer.name = "Stations"
+      layer.name = "Stations", homebutton = FALSE
     )
   m
 }
@@ -205,8 +319,8 @@ drt_station_map.drtm <- function(obj) {
 drt_save_graphics = function(x, ...) UseMethod("drt_save_graphics")
 
 drt_save_graphics.drtm <- function(obj, path) {
-  p <- drt_energy_plot(obj)
-  m <- drt_station_map(obj)
+  p <- drt_plot(obj)
+  m <- drt_map(obj)
   tmessage("Export graphics")
   mapview::mapshot(m, file = paste0(path, "/%s_i%s_station_map.png" %>% sprintf(obj$id, obj$i)),
           remove_controls = c("zoomControl", "layersControl", "homeButton"))
